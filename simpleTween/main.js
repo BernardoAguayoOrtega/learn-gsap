9d068579e5451418c4f8cc1ef94834b63e5875f1
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-25 22:04:21 -0600

    add more animations for plain js file

--- simpleTween/main.js
+++ simpleTween/main.js
@@ -1,2 +1,6 @@
-TweenMax.to('.circle', 4, { x: 150, y: 150, background: 'blue' });
-TweenMax.to('.square', 3, { x: -150, y: -150, background: 'yellow', scale: 2});
+// to values
+TweenMax.to('.circle', 2, { x: 150, y: 150, background: 'blue', ease: Back.easeInOut });
+TweenMax.to('.square', 2, { x: -150, y: -150, background: 'yellow', scale: 2, ease: Back.easeOut });
+
+// from
+TweenMax.from('#rectangle', 2, { y:200, rotation: 360, scale: -1.5 })
