9f14ff881f94c0ad9a4137cfa4f55e983a204193
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-25 21:26:43 -0600

    use gsap for react

--- simple-tween-react/src/App.js
+++ simple-tween-react/src/App.js
@@ -1,8 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { Tween } from 'react-gsap';
 
 function App() {
-	return <div className='circle'></div>;
+	useEffect(() => {}, []);
+
+	return (
+		<Tween to={{ x: 150, y: 150, background: 'blue' }} duration={2} ease='back.out(1.7)'>
+			<div className='circle' />
+		</Tween>
+	);
 }
 
 export default App;
