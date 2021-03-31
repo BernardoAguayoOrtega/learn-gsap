94694ba4521a936455d166887da78b6ce109dd27
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-30 18:15:01 -0600

    Apply same logic to react components

--- simple-tween-react/src/App.js
+++ simple-tween-react/src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
+import { TweenMax, Back } from 'gsap/all';
 
 function App() {
 	const ref = useRef(null);
 
 	useEffect(() => {
 		const element = ref.current;
-		gsap.core.Tween.to(element.querySelector('.circle'), 2, {
+		TweenMax.to(element.querySelector('.circle'), 2, {
 			x: 150,
 			y: 150,
-			background: 'blue',
-			ease: 'easeInOut',
+			background_: 'blue',
+			ease: Back.easeInOut,
+		});
+
+		TweenMax.to(element.querySelector('.square'), 2, {
+			x: -150,
+			y: -150,
+			background_: 'yellow',
+			scale: 2,
+			ease: Back.easeOut,
+		});
+
+		TweenMax.from(element.querySelector('#rectangle'), 2, {
+			y: 200,
+			rotation: 360,
+			scale: -1.5,
 		});
 	}, []);
 
 	return (
 		<div className='main' ref={ref}>
 			<div className='circle' />
+			<div className='square' />
+			<div id='rectangle' />
 		</div>
 	);
 }
--- simpleTween/main.js
+++ simpleTween/main.js
@@ -1,6 +1,17 @@
 // to values
-TweenMax.to('.circle', 2, { x: 150, y: 150, background: 'blue', ease: Back.easeInOut });
-TweenMax.to('.square', 2, { x: -150, y: -150, background: 'yellow', scale: 2, ease: Back.easeOut });
+TweenMax.to('.circle', 2, {
+	x: 150,
+	y: 150,
+	background: 'blue',
+	ease: Back.easeInOut,
+});
+TweenMax.to('.square', 2, {
+	x: -150,
+	y: -150,
+	background: 'yellow',
+	scale: 2,
+	ease: Back.easeOut,
+});
 
 // from
-TweenMax.from('#rectangle', 2, { y:200, rotation: 360, scale: -1.5 })
+TweenMax.from('#rectangle', 2, { y: 200, rotation: 360, scale: -1.5 });
